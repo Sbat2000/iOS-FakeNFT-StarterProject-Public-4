c5314de41365ea8c2dd8c84b9e158148869f356e
Shliakhin Sergei <sshlyahin@yahoo.com>
2023-07-29 16:13:44 +0300

    style: clean up

--- FakeNFTTests/ExampleUnitTests.swift
+++ FakeNFTTests/ExampleUnitTests.swift
@@ -2,7 +2,7 @@
 import XCTest
 
 final class ExampleUnitTests: XCTestCase {
-    func testExample() {
-        // TODO: - Не забудьте написать unit-тесты
-    }
+	func testExample() {
+		// TODO: - Не забудьте написать unit-тесты
+	}
 }
